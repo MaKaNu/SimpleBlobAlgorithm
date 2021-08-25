c3b36123f40946b2769a7437fcd509efa7e84971
MaKaNu <matti.kaupenjohann@fh-dortmund.de>
2021-08-25 13:18:16 +0200

    Add startup script to add the folder to MATLAB path

---
+++ startup_SimpleBlob.m
@@ -0,0 +1,8 @@
+disp("SimpleBlob Algorithm for videoLabeler: (c) Matti Kaupenjohann 2021")
+
+algPath = fileparts( mfilename('fullpath'));
+
+addpath(algPath)
+
+disp('Added Algorithm to PATH. After refreshing the algorithm list in')
+disp('the videoLabeler app it is possible to use the SimpleBlob Algorithm')
